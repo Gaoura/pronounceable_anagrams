318622838464b675a85e6e0070ff4248e7fdb1c1
Gaoura <gaoura@outlook.com>
2020-05-07 00:58:27 +0200

    Unity meta files seem important for version control

---
+++ Assets/Code/Scripts/UIPathUpdater.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIPathUpdater : MonoBehaviour
+{
+    [SerializeField]
+    private StringReference path;
+
+    [SerializeField]
+    private Text text;
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = path.value;
+    }
+}
